ed16d5402897a54f135787b0f3df419a8a5926bd
webdevkaleem <webdevkaleem@gmail.com>
2025-03-14 13:28:01 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Uploadthing UI - Documentation 
+
+<img src="./public/uploadthing-ui-hero.png">
+
+| Documentation For: https://github.com/webdevkaleem/uploadthing-ui
---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import { buttonVariants } from "@/components/ui/button";
+import { page_routes } from "@/lib/routes-config";
+import { MoveUpRightIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex sm:min-h-[85.5vh] min-h-[82vh] flex-col sm:items-center justify-center text-center px-2 sm:py-8 py-12">
+      <Link
+        href="https://github.com/webdevkaleem/uploadthing-ui-docs"
+        target="_blank"
+        className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4 sm:-mt-12"
+      >
+        Follow along on GitHub{" "}
+        <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
+      </Link>
+      <h1 className="text-[2.4rem] leading-10 sm:leading-[4.5rem] font-bold mb-4 sm:text-6xl text-left sm:text-center">
+        Make your uploadthing components{" "}
+        <span className="text-primary">better</span>
+      </h1>
+      <p className="mb-8 sm:text-lg max-w-[800px] text-muted-foreground text-left sm:text-center">
+        A set of beautifully-designed, accessible uploadthing components.
+        Currently works with only Next / React. Open Source. Open Code.{" "}
+      </p>
+      <div className="sm:flex sm:flex-row grid grid-cols-2 items-center sm;gap-5 gap-3 mb-8">
+        <Link
+          href={`/docs${page_routes[0].href}`}
+          className={buttonVariants({ className: "px-6", size: "lg" })}
+        >
+          Get Stared
+        </Link>
+      </div>
+    </div>
+  );
+}
