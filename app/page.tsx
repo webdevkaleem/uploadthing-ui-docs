4cace74953c398c8085f799f7279b64c3a491650
webdevkaleem <webdevkaleem@gmail.com>
2025-03-21 05:55:38 +0500

    fix - abit more descriptive docs, removed uselessish content

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,11 @@ export default function Home() {
         <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
       </Link>
       <h1 className="text-[2.4rem] leading-10 sm:leading-[4.5rem] font-bold mb-4 sm:text-6xl text-left sm:text-center">
-        Make your uploadthing components{" "}
-        <span className="text-primary">better</span>
+        Build your uploadthing <span className="text-primary">components</span>
       </h1>
       <p className="mb-8 sm:text-lg max-w-[800px] text-muted-foreground text-left sm:text-center">
         A set of beautifully-designed, accessible uploadthing components.
-        Currently works with only Next / React. Open Source. Open Code.{" "}
+        Currently works with only Next / React. Open Source. Open Code.
       </p>
       <div className="sm:flex sm:flex-row grid grid-cols-2 items-center sm;gap-5 gap-3 mb-8">
         <Link
